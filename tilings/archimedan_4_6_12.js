a329bbcd44845ec77e17d76824b0d5170c18b6c9
ynasser <ynasser@uwaterloo.ca>
2015-10-26 00:14:16 -0400

    backing up calculations

--- tilings/archimedan_4_6_12.js
+++ tilings/archimedan_4_6_12.js
@@ -16,23 +16,64 @@ window.onload = function() {
                }
             }
             return raphael.path("M " + shape);
-        }   
+        }
+
+        for (j = -1; j < 3; j++){  
+            //dodecagon
+            make_regular_polygon(150+(j*(180+32.5)), 150-(j*57), 90, 12);
+            console.log("hi " + j);
+            console.log("dssshi");
+
+            //bottom_right_square
+            make_regular_polygon(228+(j*(180+32.5)), 228-(j*57), 32.5, 4);
+            
+            //top_left_square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4);
+            // middle_right_square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r210T184.5,49.5");
+            // top right square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r60T106.5,-28");
+            // middle left square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-60T-28,106.5");
+            // bottom left square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-210T49,184");
+        }
+        for (j = -1; j < 3; j++){  
+            //dodecagon
+            make_regular_polygon(150+(j*(180+32.5)), 150-(j*57), 90, 12);
+            console.log("hi " + j);
+            console.log("dssshi");
+
+            //bottom_right_square
+            make_regular_polygon(228+(j*(180+32.5)), 228-(j*57), 32.5, 4);
+            
+            //top_left_square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4);
+            // middle_right_square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r210T184.5,49.5");
+            // top right square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r60T106.5,-28");
+            // middle left square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-60T-28,106.5");
+            // bottom left square
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-210T49,184");
+        }
+       /** 
         //dodecagon
-        make_regular_polygon(150, 150, 90, 12);
+        make_regular_polygon(150+180+32.5, 150-57, 90, 12);
         //bottom_right_square
-        make_regular_polygon(228, 228, 32.5, 4);
+        make_regular_polygon(228+180+32.5, 228-57, 32.5, 4);
         //top_left_square
-        make_regular_polygon(72,72,32.5,4);
+        make_regular_polygon(72+180+32.5,72-57,32.5,4);
         // middle_right_square
-        make_regular_polygon(72,72,32.5,4).transform("r210T184.5,49.5");
+        make_regular_polygon(72+180+32.5,72-57,32.5,4).transform("r210T184.5,49.5");
         // top right square
-        make_regular_polygon(72,72,32.5,4).transform("r60T106.5,-28");
+        make_regular_polygon(72+180+32.5,72-57,32.5,4).transform("r60T106.5,-28");
         // middle left square
-        make_regular_polygon(72,72,32.5,4).transform("r-60T-28,106.5");
+        make_regular_polygon(72+180+32.5,72-57,32.5,4).transform("r-60T-28,106.5");
         // bottom left square
-        make_regular_polygon(72,72,32.5,4).transform("r-210T49,184");
-        
+        make_regular_polygon(72+180+32.5,72-57,32.5,4).transform("r-210T49,184");
 
         //hexagon = make_regular_polygon(150,150,30,6);
-
+**/
 }
