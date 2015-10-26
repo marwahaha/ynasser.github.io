316de71c3196a84ac0a65cbc85d9f2f5f06533d3
ynasser <ynasser@uwaterloo.ca>
2015-10-26 01:17:17 -0400

    backing up calculations

--- tilings/archimedan_4_6_12.js
+++ tilings/archimedan_4_6_12.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-        var raphael = Raphael(document.getElementById('canvas_container_4_6_12'), 480, 480),
+        var raphael = Raphael(document.getElementById('canvas_container_4_6_12'), 900, 900),
                     dashed = {fill: "none", stroke: "#666", "stroke-dasharray": "- "};
         
         function make_regular_polygon(x,y,r,n){
@@ -20,43 +20,35 @@ window.onload = function() {
 
         for (j = -1; j < 3; j++){  
             //dodecagon
-            make_regular_polygon(150+(j*(180+32.5)), 150-(j*57), 90, 12);
-            console.log("hi " + j);
-            console.log("dssshi");
-
+            make_regular_polygon(150+(j*(180+32.5)), 150-(j*57), 90, 12).attr({"fill":"blue"});
             //bottom_right_square
-            make_regular_polygon(228+(j*(180+32.5)), 228-(j*57), 32.5, 4);
-            
+            make_regular_polygon(228+(j*(180+32.5)), 228-(j*57), 32.5, 4).attr({"fill":"red"});
             //top_left_square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4);
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).attr({"fill":"red"});
             // middle_right_square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r210T184.5,49.5");
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r210T184.5,49.5").attr({"fill":"red"});
             // top right square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r60T106.5,-28");
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r60T106.5,-28").attr({"fill":"red"});
             // middle left square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-60T-28,106.5");
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-60T-28,106.5").attr({"fill":"red"});
             // bottom left square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-210T49,184");
+            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-210T49,184").attr({"fill":"red"});
         }
-        for (j = -1; j < 3; j++){  
+        for (k = -1; k < 3; k++){  
             //dodecagon
-            make_regular_polygon(150+(j*(180+32.5)), 150-(j*57), 90, 12);
-            console.log("hi " + j);
-            console.log("dssshi");
-
+            make_regular_polygon(150-(k*57), 150+(k*212.5), 90, 12).attr({"fill":"blue"});
             //bottom_right_square
-            make_regular_polygon(228+(j*(180+32.5)), 228-(j*57), 32.5, 4);
-            
+            make_regular_polygon(228-(k*57), 228+(k*212.5), 32.5, 4).attr({"fill":"red"});
             //top_left_square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4);
+            make_regular_polygon(72-(k*57),72+(k*212.5),32.5,4).attr({"fill":"red"});
             // middle_right_square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r210T184.5,49.5");
+            make_regular_polygon(72-(k*57),72+(k*212.5),32.5,4).transform("r210T184.5,49.5").attr({"fill":"red"});
             // top right square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r60T106.5,-28");
+            make_regular_polygon(72-(k*57),72+(k*212.5),32.5,4).transform("r60T106.5,-28").attr({"fill":"red"});
             // middle left square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-60T-28,106.5");
+            make_regular_polygon(72-(k*57),72+(k*212.5),32.5,4).transform("r-60T-28,106.5").attr({"fill":"red"});
             // bottom left square
-            make_regular_polygon(72+(j*(180+32.5)),72-(j*57),32.5,4).transform("r-210T49,184");
+            make_regular_polygon(72-(k*57),72+(k*212.5),32.5,4).transform("r-210T49,184").attr({"fill":"red"});
         }
        /** 
         //dodecagon
