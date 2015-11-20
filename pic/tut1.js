a44aa879798b064dc383ab0a9aa6ff7c229e748a
ynasser <ynasser@uwaterloo.ca>
2015-11-19 19:48:15 -0500

    backing up polygons

---
+++ pic/tut1.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {	
+    // 3.12.12, 4.6.12, 4.8.8, 6.6.6, and rosette dual tilings 10RD and 8RD
+
+    var canvas = new fabric.StaticCanvas('c');
+
+	var rect = new fabric.Rect({
+          left: 100,
+          top: 100,
+          fill: 'red',
+          width: 20,
+          height: 20,
+          angle:45
+    });
+
+    function make_regular_polygon(x,y,r,n){
+        var shape = "";
+        
+        for (i = 0; i < n; i++) {
+           shape_x = x + r * Math.cos(2 * Math.PI * i / n); 
+           shape_y = y + r * Math.sin(2 * Math.PI * i / n);
+           
+           if (i==n-1){
+               shape = shape + " " + shape_x + " " + shape_y + " z";
+           } else {
+               shape = shape + " " + shape_x + " " + shape_y + " L ";
+           }
+        }
+        var path = new fabric.Path("M " + shape);
+        return path;
+    }
+    
+    // 3.12.12
+    for(p=-1;p<6;p++){
+        for(q=0;q<6;q++){
+            var dodecagon_1 = make_regular_polygon(250+145.5*p,220+148*q,75,12);
+            dodecagon_1.set({
+                fill:'red',
+                stroke:'green',
+                angle:75
+            })
+            canvas.add(dodecagon_1);
+    
+            var dodecagon_2 = make_regular_polygon(175.5+145.5*p,24.5,75,12);
+            dodecagon_2.set({
+                fill:'blue',
+                stroke:'green',
+                angle:75
+            })
+            canvas.add(dodecagon_2);
+            
+            var triangle = make_regular_polygon(132+145.5*p,80,23,3);
+            triangle.set({
+                fill:'black', 
+                stroke:'green',
+                angle:30
+            })
+            canvas.add(triangle);
+        }
+    }
+  
+    // 3^6
+    /**
+    for(l = 0;l < 6; l++){
+        for(j = 0;j<6; j++){
+            for(k=0;k<5;k++){
+                 
+                var triangle = make_regular_polygon(224*j,130.5*l,75,3);
+                triangle.set({
+                    fill:'white', 
+                    stroke:'green',
+                    transformMatrix:[1,0,0,1,0,0], 
+                    angle:(k*60),
+                    //stroke-width:"6"
+                });
+                canvas.add(triangle);
+                
+            }
+        }
+    }
+    **/
+
+});
