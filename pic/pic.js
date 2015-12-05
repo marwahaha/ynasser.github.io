9da81f4de96bc76bef6b3f21adce05c2f694f80a
ynasser <ynasser@uwaterloo.ca>
2015-12-04 19:55:12 -0500

    adding tiling 4.6.12

--- pic/pic.js
+++ pic/pic.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
         }
 
         if (tiling == "4_6_12"){
-                render_4_6_12(theta);//, display_base);
+                render_4_6_12(theta, stroke_colour);//, display_base);
         } else if (tiling == "4_8_8"){
                 render_4_8_8(theta, stroke_colour);
         } else if (tiling == "6_6_6"){
@@ -196,7 +196,7 @@ $(document).ready(function() {
             canvas.add(plane);
     }
 
-    function render_4_6_12(theta){
+    function render_4_6_12(theta, stroke_colour){
         // making the dodecagon
         var centre = [0,0];
         var n = 12;
@@ -206,15 +206,18 @@ $(document).ready(function() {
         var dodecagon = make_regular_polygon(centre[0], centre[1], radius, n);
         dodecagon.set({
             fill:"rgba(0,0,0,0)",
-            stroke:'green',
+            stroke:stroke_colour,
             strokeWidth:1,
         })
         var dodecagon_motif = make_motif(dodecagon, theta, vertices, midpoints, centre, n)
-        var dodecagon = new fabric.Group([dodecagon]);
+            
+        var dodecagon = new fabric.Group([dodecagon_motif]);
+        dodecagon.set({
+            left:175,
+            top:176
+        })
 
-        canvas.add(dodecagon_motif);
         // making the squares (3)
-        // square's radius must be 27.4519, b/c the dodecagon's side length is 38.8229
         var centre = [0,0];
         var n = 4;
         var radius = 27.4519;
@@ -223,32 +226,122 @@ $(document).ready(function() {
         var square_1 = make_regular_polygon(centre[0], centre[1], radius, n);
         square_1.set({
             fill:"rgba(0,0,0,0)",
-            stroke:'green',
+            stroke:stroke_colour,
             strokeWidth:1,
         })
-        var square_motif_1 = make_motif(square, theta, vertices, midpoints, centre, n)
+        var square_motif_1 = make_motif(square_1, theta, vertices, midpoints, centre, n)
         var square_1 = new fabric.Group([square_motif_1]);
+        square_1.set({
+            left:110,
+            top:110
+        })
 
         var square_2 = make_regular_polygon(centre[0], centre[1], radius, n);
         square_2.set({
             fill:"rgba(0,0,0,0)",
-            stroke:'green',
+            stroke:stroke_colour,
             strokeWidth:1,
         })
-        var square_motif_2 = make_motif(square, theta, vertices, midpoints, centre, n)
+        var square_motif_2 = make_motif(square_2, theta, vertices, midpoints, centre, n)
         var square_2 = new fabric.Group([square_motif_2]);
-        
+        square_2.set({
+            left:86,
+            top:200,
+        })
+        square_2.setAngle(30);
+
         var square_3 = make_regular_polygon(centre[0], centre[1], radius, n);
         square_3.set({
             fill:"rgba(0,0,0,0)",
-            stroke:'green',
+            stroke:stroke_colour,
             strokeWidth:1,
         })
-        var square_motif_3 = make_motif(square, theta, vertices, midpoints, centre, n)
+        var square_motif_3 = make_motif(square_3, theta, vertices, midpoints, centre, n)
         var square_3 = new fabric.Group([square_motif_3]);
-        
+        square_3.set({
+            left:150.5,
+            top:264,
+        })   
+        square_3.setAngle(60);
+
         // making the hexagons (3)
-        // var radius = 38.8229;
+        var n = 6;
+        var radius = 38.8229;
+        var vertices = determine_vertices(centre[0], centre[1], radius, n);
+        var midpoints = determine_midpoints(vertices);
+        var hexagon_1 = make_regular_polygon(centre[0], centre[1], radius, n);
+        hexagon_1.set({
+            fill:"rgba(0,0,0,0)",
+            stroke:stroke_colour,
+            strokeWidth:1,
+        })
+        var hexagon_motif_1 = make_motif(hexagon_1, theta, vertices, midpoints, centre, n)
+        var hexagon_1 = new fabric.Group([hexagon_motif_1]);
+        hexagon_1.set({
+            left:72,
+            top:148
+        })
+        hexagon_1.setAngle(45);
+
+        var hexagon_2 = make_regular_polygon(centre[0], centre[1], radius, n);
+        hexagon_2.set({
+            fill:"rgba(0,0,0,0)",
+            stroke:stroke_colour,
+            strokeWidth:1,
+        })
+        var hexagon_motif_2 = make_motif(hexagon_2, theta, vertices, midpoints, centre, n)
+        var hexagon_2 = new fabric.Group([hexagon_motif_2]);
+        hexagon_2.set({
+            left:99.5,
+            top:251
+        })
+        hexagon_2.setAngle(45);
+        
+        var hexagon_3 = make_regular_polygon(centre[0], centre[1], radius, n);
+        hexagon_3.set({
+            fill:"rgba(0,0,0,0)",
+            stroke:stroke_colour,
+            strokeWidth:1,
+        })
+        var hexagon_motif_3 = make_motif(hexagon_3, theta, vertices, midpoints, centre, n)
+        var hexagon_3 = new fabric.Group([hexagon_motif_3]);
+        hexagon_3.set({
+            left:202,
+            top:278.5
+        })
+        hexagon_3.setAngle(45);
+        
+        var translational_unit = new fabric.Group([dodecagon, hexagon_1, hexagon_2, hexagon_3, square_1, square_2, square_3]);
+        translational_unit.setAngle(45);
+        //canvas.add(translational_unit);
+            
+        var column = new fabric.Group();
+        for (var k = -1; k < 3; k++){
+                var tmp_group_0 = new fabric.Group([translational_unit]);
+                tmp_group_0.set({
+                        left:100,
+                        top:184*k
+                });
+                
+                var tmp_group_1 = new fabric.Group([translational_unit]);
+                tmp_group_1.set({
+                        left:260,
+                        top:92+184*k
+                });
+                column.add(tmp_group_0);
+                column.add(tmp_group_1);
+        }
+        //canvas.add(column);
+        var plane = new fabric.Group();
+        for (var j = -2; j < 4; j++){
+                var tmp_group = new fabric.Group([column]);
+                tmp_group.set({
+                        left:320*j,
+                        top:96
+                });
+                plane.add(tmp_group);
+        }
+        canvas.add(plane);
       }
 
     function render_4_8_8(theta, stroke_colour){
@@ -304,7 +397,7 @@ $(document).ready(function() {
             var translational_unit = new fabric.Group([octagon_1, square_1, square_2]);
 
             var column = new fabric.Group();
-            for (var k = -1; k < 3; k++){
+            for (var k = -5; k < 4; k++){
                     var tmp_group_0 = new fabric.Group([translational_unit]);
                     tmp_group_0.set({
                             left:100,
@@ -321,7 +414,7 @@ $(document).ready(function() {
             }
             
             var plane = new fabric.Group();
-            for (var j = -2; j < 4; j++){
+            for (var j = -3; j < 5; j++){
                     var tmp_group = new fabric.Group([column]);
                     tmp_group.set({
                             left:364.5*j,
